f91603fe74221a67fe1d7f14ee6519e1784517a0
aadilahdarus-wq <aadilahdarus@gmail.com>
2026-06-09 14:17:10 +0800

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,6 @@
 import { getProfile } from "@/sanity/sanity.query";
 import type { ProfileType } from "@/types";
 import HeroSvg from "./icons/HeroSvg";
-import Job from "./components/Job";
 
 export default async function Home() {
   const profile: ProfileType[] = await getProfile();
@@ -12,32 +11,16 @@ export default async function Home() {
         {profile &&
           profile.map((data) => (
             <div key={data._id} className="lg:max-w-2xl max-w-2xl">
-              <h1 className="text-3xl font-bold tracking-tight sm:text-5xl mb-6 lg:leading-[3.7rem] leading-tight lg:min-w-[700px] min-w-full">
+              <h1 className="text-3xl font-bold tracking-tight sm:text-5xl mb-6 lg:leading-[3.7rem] leading-tight">
                 {data.headline}
               </h1>
               <p className="text-base text-zinc-400 leading-relaxed">
                 {data.shortBio}
               </p>
-              <ul className="flex items-center gap-x-6 my-10">
-                {Object.entries(data.socialLinks)
-                  .sort()
-                  .map(([key, value], id) => (
-                    <li key={id}>
-                      <a
-                        href={value}
-                        rel="noreferer noopener"
-                        className="flex items-center gap-x-3 mb-5 hover:text-purple-400 duration-300"
-                      >
-                        {key[0].toUpperCase() + key.toLowerCase().slice(1)}
-                      </a>
-                    </li>
-                  ))}
-              </ul>
             </div>
           ))}
         <HeroSvg />
       </section>
-      <Job />
     </main>
   );
 }
