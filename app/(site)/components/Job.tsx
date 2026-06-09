15f03eed876f732d9c58b04fb81b4586f7e2966d
aadilahdarus-wq <aadilahdarus@gmail.com>
2026-06-09 14:15:17 +0800

    Update Job.tsx

--- app/(site)/components/Job.tsx
+++ app/(site)/components/Job.tsx
@@ -15,19 +15,10 @@ export default async function Job() {
 
       <div className="flex flex-col gap-y-12">
         {job.map((data) => (
-          <div
-            key={data._id}
-            className="flex items-start lg:gap-x-6 gap-x-4 max-w-2xl"
-          >
+          <div key={data._id} className="flex items-start lg:gap-x-6 gap-x-4 max-w-2xl">
             {data.logo && (
-              <a href={data.url} rel="noreferrer noopener"
-                className="min-h-[60px] min-w-[60px] rounded-md overflow-clip relative">
-                <Image src={data.logo} className="object-cover"
-                  alt={`${data.name} logo`} fill />
+              <a href={data.url} rel="noreferrer noopener" className="min-h-[60px] min-w-[60px] rounded-md overflow-clip relative">
+                <Image src={data.logo} className="object-cover" alt={`${data.name} logo`} fill />
               </a>
             )}
             <div className="flex flex-col items-start">
-              <h3 className="text-xl font-bold">{data.name}</h3>
-              <p>{data.jobTitle}</p>
-              <small className="text-sm text-zinc-500 mt-2 tracking-widest uppercase">
-                {data.startDate ? new Date(data.startDate).getFullYear() : ""} - {data.endDate ? new Date(data.endDate).getFullYear() : "Present"}
